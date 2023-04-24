3e2ed934686fed1e96012c668f949fe5fd0277f7
Oren Mittman <nihil2501@gmail.com>
2023-04-24 02:05:11 -0400

    wip

--- lib/footgauntlet/utils/pubsub.rb
+++ lib/footgauntlet/utils/pubsub.rb
@@ -2,21 +2,21 @@
 
 module Pubsub
   class << self
-    def subscribe(topic, &block)
-      topic_subscribers[topic] << block
+    def subscribe(topic, &subscription)
+      topic_subscriptions[topic] << subscription
     end
 
     def publish(topic, record)
-      subscribers = topic_subscribers[topic]
-      subscribers.each do |block|
-        block.call(record)
+      subscriptions = topic_subscriptions[topic]
+      subscriptions.each do |subscription|
+        subscription.(record)
       end
     end
 
     private
 
-    def topic_subscribers
-      @topic_subscribers ||=
+    def topic_subscriptions
+      @topic_subscriptions ||=
         Hash.new do |memo, topic|
           memo[topic] = []
         end

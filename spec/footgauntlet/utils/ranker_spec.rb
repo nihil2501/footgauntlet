262e98df8f788c75f048189a13fd62f6f8339843
Oren Mittman <nihil2501@gmail.com>
2023-04-26 03:54:42 -0400

    simplify ranker spec

--- spec/footgauntlet/utils/ranker_spec.rb
+++ spec/footgauntlet/utils/ranker_spec.rb
@@ -15,71 +15,82 @@ describe Ranker do
   end
 
   describe "#rank" do
-    Rankable =
-      Data.define(:id, :points, :tiebreaker) do
-        def <=>(other)
-          self.points <=> other.points
-        end
-      end
-
     before do
-      @enumerable = [
-        @rankable_a = Rankable.new(id: :a, points: 1, tiebreaker: 1),
-        @rankable_b = Rankable.new(id: :b, points: 2, tiebreaker: 1),
-        @rankable_c = Rankable.new(id: :c, points: 3, tiebreaker: 1),
-        @rankable_d = Rankable.new(id: :d, points: 1, tiebreaker: 2),
-        @rankable_e = Rankable.new(id: :e, points: 2, tiebreaker: 2),
-        @rankable_f = Rankable.new(id: :f, points: 3, tiebreaker: 2),
-        @rankable_g = Rankable.new(id: :g, points: 1, tiebreaker: 3),
-        @rankable_h = Rankable.new(id: :h, points: 2, tiebreaker: 3),
-        @rankable_i = Rankable.new(id: :i, points: 3, tiebreaker: 3),
-      ]
+      @enumerable = 1..9
+    end
 
-      @ranker =
-        Ranker.new do |definition|
-          definition.inner_compare = -> { _2.tiebreaker <=> _1.tiebreaker }
-          definition.map = -> (rankable, rank) { [rankable, rank] }
+    describe "with compares specified" do
+      before do
+        @ranker =
+          Ranker.new do |definition|
+            definition.compare = -> { _1 / 3 <=> _2 / 3 }
+            definition.inner_compare = -> { _2 <=> _1 }
+            definition.map = -> (object, rank) { [rank, object] }
+          end
+      end
+
+      describe "with `count` argument provided" do
+        before do
+          @args = [4]
         end
+
+        it "gives back the top `count` ranked" do
+          expected = [
+            [1, 9],
+            [2, 6], [2, 7], [2, 8],
+          ]
+
+          actual = @ranker.rank(@enumerable, *@args)
+
+          assert_equal(
+            expected,
+            actual
+          )
+        end
+      end
+
+      describe "without `count` argument provided" do
+        before do
+          @args = []
+        end
+
+        it "gives back all ranked" do
+          expected = [
+            [1, 9],
+            [2, 6], [2, 7], [2, 8],
+            [5, 3], [5, 4], [5, 5],
+            [8, 1], [8, 2],
+          ]
+
+          actual = @ranker.rank(@enumerable, *@args)
+
+          assert_equal(
+            expected,
+            actual
+          )
+        end
+      end
     end
 
-    describe "with `count` argument provided" do
+    describe "with compares unspecified" do
       before do
-        @args = [4]
+        @ranker =
+          Ranker.new do |definition|
+            definition.map = -> (object, rank) { [rank, object] }
+          end
       end
 
-      it "gives back the top `count` ranked" do
+      it "uses trivial compare" do
         expected = [
-          [@rankable_c, 1],
-          [@rankable_f, 1],
-          [@rankable_i, 1],
-          [@rankable_b, 4],
-        ]
-
-        actual = @ranker.rank(@enumerable, *@args)
-
-        assert_equal(
-          expected,
-          actual
-        )
-      end
-    end
-
-    describe "without `count` argument provided" do
-      before do
-        @args = []
-      end
-
-      it "gives back all ranked" do
-        expected = [
-          [@rankable_c, 1],
-          [@rankable_f, 1],
-          [@rankable_i, 1],
-          [@rankable_b, 4],
-          [@rankable_e, 4],
-          [@rankable_h, 4],
-          [@rankable_a, 7],
-          [@rankable_d, 7],
-          [@rankable_g, 7],
+          [1, 9],
+          [2, 8],
+          [3, 7],
+          [4, 6],
+          [5, 5],
+          [6, 4],
+          [7, 3],
+          [8, 2],
+          [9, 1],
         ]
 
         actual = @ranker.rank(@enumerable, *@args)

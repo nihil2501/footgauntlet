5ff7d8f5015fe086457fd8df6d26c5f62e11d12a
Oren Mittman <nihil2501@gmail.com>
2023-04-26 20:14:22 -0400

    add read only struct comment

--- lib/footgauntlet/utils/read_only_struct.rb
+++ lib/footgauntlet/utils/read_only_struct.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ReadOnlyStruct < Struct
+  # This is mostly supplanted in newer Ruby with the `Data` builtin that makes
+  # a value struct with no setters and frozen state. However, `Data` made it
+  # hard to memoize pure derivations using ivars because of the frozen state so
+  # I had to fall back to this thing.
   class << self
     def new(*)
       super(*, keyword_init: true).tap do |klass|

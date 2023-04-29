8190d63e3a9553f3f2c63abaef01c455ed26e6c1
Oren Mittman <nihil2501@gmail.com>
2023-04-29 16:54:51 -0400

    Revert "rdoc unique_run_tracker"
    This reverts commit d290efca7a1b10bd989fc53748c12eadba06b50d.

--- lib/footgauntlet/core/processors/league_summary_processor/unique_run_tracker.rb
+++ lib/footgauntlet/core/processors/league_summary_processor/unique_run_tracker.rb
@@ -3,26 +3,14 @@
 module Footgauntlet
   module Core
     class LeagueSummaryProcessor
-      ##
-      # This class tracks unique runs.
       class UniqueRunTracker
-        ##
-        # Returns the current count of unique runs.
-        # @return [Integer] the unique run count
         attr_reader :count
 
-        ##
-        # Initializes a new `UniqueRunTracker`
         def initialize
           @unique_run = Set[]
           @count = 0
         end
 
-        ##
-        # Checks if the given set of items completes a unique run.
-        #
-        # @param items [Set] the set of items to check
-        # @return [Boolean] true if the unique run is complete, false otherwise
         def complete?(items)
           # Interesting note: operand order already optimized with respect to
           # cardinality by implementation of `Set#intersect?`:
@@ -33,8 +21,6 @@ module Footgauntlet
           end
         end
 
-        ##
-        # Completes the current unique run, incrementing the `count`.
         def complete!
           @unique_run.clear
           @count += 1
